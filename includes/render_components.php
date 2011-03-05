5777083da852294a1cb0dcf2afabd1cedb511e68
Jesse von Doom <jesse@cashmusic.org>
2011-03-05 01:21:25 -0800

    added a check to make sure the app was running on the correct FB page.

--- includes/render_components.php
+++ includes/render_components.php
@@ -1,15 +1,19 @@
 <?php if ($on_facebook_tab) { ?>
 	<div id="messagespc">
 		<?php
-		if ($fb_signed_request['page']['liked']) {
-			$_SESSION['fb_reports_liked'] = true;
-			echo '<p>' . str_replace("'","\'",ISAFAN_MESSAGE) . '</p>' .
-			'<a href="claimasset.php?approot=' . $currenturl . '" class="biglink">' . DOWNLOAD_TITLE . '</a>' .
-			'<p class="smallmessage">' .
-			'<br />Enjoy it, and <a id="fbsharelink" href="http://www.facebook.com/sharer.php?u=' . urlencode($currenturl) . '">please share</a>!</p>';
+		if ($fb_signed_request['page']['id'] == FACEBOOK_FANPAGE_ID) {
+			if ($fb_signed_request['page']['liked']) {
+				$_SESSION['fb_reports_liked'] = true;
+				echo '<p>' . str_replace("'","\'",ISAFAN_MESSAGE) . '</p>' .
+				'<a href="claimasset.php?approot=' . $currenturl . '" class="biglink">' . DOWNLOAD_TITLE . '</a>' .
+				'<p class="smallmessage">' .
+				'<br />Enjoy it, and <a id="fbsharelink" href="http://www.facebook.com/sharer.php?u=' . urlencode($currenturl) . '">please share</a>!</p>';
+			} else {
+				$_SESSION['fb_reports_liked'] = false;
+				echo '<span class="alert">To get your download please "like" this page by hitting the like button near the title.</span>';
+			}
 		} else {
-			$_SESSION['fb_reports_liked'] = false;
-			echo '<span class="alert">To get your download please "like" this page by hitting the like button near the title.</span>';
+			echo 'Get your download at <a href="'. FACEBOOK_FANPAGE_URL . '" target="_new">The ' . FACEBOOK_FANPAGE_NAME . ' page</a>.';
 		}
 		?>
 	</div>
